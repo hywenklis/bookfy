1236fda788ec0d092361415176f38e993f22ad9a
hywenklis <hywenklis@hotmail.com>
2023-05-10 21:27:31 -0300

    Ajustes na tela de detalhes do livro

--- src/screens/BookDetailsScreen.tsx
+++ src/screens/BookDetailsScreen.tsx
@@ -1,20 +1,31 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, TextInput } from 'react-native';
 import moment from 'moment';
+import books from '../data/books.json';
 
 const BookDetailsScreen = ({ route, navigation }) => {
     const { book } = route.params;
-    const [isAvailable, setIsAvailable] = useState(!book.borrowed);
-    const [returnDate, setReturnDate] = useState(moment(book.returnDate).toDate());
-    const [duration, setDuration] = useState('');
+    const currentBook = books.find(b => b.id === book.id);
+    const [isAvailable, setIsAvailable] = useState(!currentBook.borrowed);
+    const [returnDate, setReturnDate] = useState(moment(currentBook.returnDate).toDate());
+    const remainingTime = moment(currentBook.returnDate).diff(moment());
+    const remainingDays = moment.duration(remainingTime).asDays();
+    const [duration, setDuration] = useState(Math.ceil(remainingDays));
 
     const handleBorrow = () => {
         setIsAvailable(false);
-        setReturnDate(moment().add(duration, 'days').toDate());
+        const newReturnDate = moment().add(duration, 'days').toDate();
+        setReturnDate(newReturnDate);
+        currentBook.borrowed = true;
+        currentBook.returnDate = newReturnDate;
     };
 
     const handleReturn = () => {
+        const newDate = moment().add(duration, 'days').toDate()
         setIsAvailable(true);
+        currentBook.borrowed = false;
+        currentBook.returnDate = newDate;
+        setDuration('');
     };
 
     const getRemainingTime = () => {
@@ -35,7 +46,7 @@ const BookDetailsScreen = ({ route, navigation }) => {
                     <Text>O livro está disponível para empréstimo.</Text>
                     <TextInput
                         style={styles.input}
-                        value={duration}
+                        value={duration.toString()}
                         onChangeText={setDuration}
                         placeholder="Duração do empréstimo em dias"
                     />
@@ -56,40 +67,38 @@ const BookDetailsScreen = ({ route, navigation }) => {
     );
 };
 
-const styles = StyleSheet.create
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: 'gray',
+        padding: 10,
+        borderRadius: 5,
+        marginBottom: 10,
+        alignSelf: 'stretch',
+    },
+    button: {
+        backgroundColor: '#2196F3',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+        alignSelf: 'center',
+        marginTop: 10,
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+});
 
-    ({
-        container: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            paddingHorizontal: 20,
-        },
-        title: {
-            fontSize: 24,
-            fontWeight: 'bold',
-            marginBottom: 20,
-        },
-        input: {
-            borderWidth: 1,
-            borderColor: 'gray',
-            padding: 10,
-            borderRadius: 5,
-            marginBottom: 10,
-            alignSelf: 'stretch',
-        },
-        button: {
-            backgroundColor: '#2196F3',
-            paddingVertical: 10,
-            paddingHorizontal: 20,
-            borderRadius: 5,
-            alignSelf: 'center',
-            marginTop: 10,
-        },
-        buttonText: {
-            color: '#fff',
-            fontWeight: 'bold',
-        },
-    });
-
-export default BookDetailsScreen;
+export default BookDetailsScreen;    
